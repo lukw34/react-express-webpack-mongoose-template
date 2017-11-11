4ff7f17d4296d4547714ea9e21a8e839976e5365
lukw34 <lukw34@gmail.com>
2017-11-11 23:51:53 +0100

    Routing fix and add md color style

--- app/server/Bootstrap.js
+++ app/server/Bootstrap.js
@@ -1,64 +1,72 @@
+const Logger = require('./utils/Logger'),
+    Express = require('express'),
+    bodyParser = require('body-parser'),
+    path = require('path'),
+    winston = require('winston'),
+    mongoose = require('mongoose'),
+    winstonRequestLogger = require('winston-request-logger'),
+    MongooseUtils = require('./utils/Mongo'),
+    routes = require('./routes'),
+    ErrorController = require('./controllers/ErrorController');
+
 class Bootstrap {
     constructor() {
-        const Logger = require('./utils/Logger');
-        this.Express = require('express');
-        this.bodyParser = require('body-parser');
-        this.path = require('path');
-        this.MongooseUtils = require('./utils/Mongo');
-        this.ErrorController = require('./controllers/ErrorController');
-        const winston = require('winston');
+        this.Express = Express;
+        this.bodyParser = bodyParser;
+        this.MongooseUtils = MongooseUtils;
+        this.ErrorController = ErrorController;
         this.winstonLogger = new (winston.Logger)({transports: [new (winston.transports.Console)({colorize: true})]});
-
+        this.publicPath = path.join(__dirname, '/../../public');
 
         this.port = process.env.PORT || 9004;
-        this.mongo = 'mongodb://localhost/wotk_cost_calculator';
+        this.mongo = 'mongodb://localhost/template_db';
 
         //global variable
         global.logger = new Logger();
-        global.Mongo = new this.MongooseUtils(require('mongoose'));
-
+        global.Mongo = new this.MongooseUtils(mongoose);
     }
 
     run() {
         global.logger.log('Run application ...');
 
         global.Mongo.connect(this.mongo).then(() => {
-            this._startExpress(require('./routes'))
+            this._startExpress(routes);
         }).catch(({message}) => {
-           global.logger.error(message);
-
+            global.logger.error(message);
         });
     }
 
-    _startExpress(routes) {
+    _startExpress(routing) {
         const app = new this.Express();
 
-
-        app.use(require('winston-request-logger').create(this.winstonLogger, {
-            'method': ':method',
-            'url': ':url[pathname]'
+        app.use(winstonRequestLogger.create(this.winstonLogger, {
+            method: ':method',
+            url: ':url[pathname]'
         }));
 
         app.use(this.bodyParser.json());
         app.use(this.bodyParser.urlencoded({extended: false}));
-        app.use(this.Express.static(this.path.join(__dirname, '/../../public')));
+        app.use(this.Express.static(this.publicPath));
+
+        app.get('*', (req, res) => {
+            res.sendFile(path.join(this.publicPath, 'index.html'));
+        });
+
         app.use(this.ErrorController.errorHandler);
-        app.use('/', routes);
+        app.use('/', routing);
 
         this.server = app.listen(this.port, err => {
             if (err) {
                 global.logger.error('Application runtime error !');
             } else {
-                global.logger.log(`Application start on port: ${this.port}`)
+                global.logger.log(`Application start on port: ${this.port}`);
             }
         });
 
-        this.server.on('error', err => {
+        this.server.on('error', () => {
             global.logger.error('Application runtime error !');
             process.exit(1);
-        })
-
-
+        });
     }
 }
 
