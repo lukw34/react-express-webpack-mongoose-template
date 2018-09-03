0dc62682ddf0f1e5d0edcc661eb569ba01145593
lukw3 <lukw34@gmail.com>
2018-09-03 20:25:57 +0200

    Add docker push script

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,12 @@
-echo $TRAVIS_TAG
-echo $DOCKER_USERNAME
-echo $DOCKER_PASSWORD
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+| sed 's/[",]//g')
+echo $PACKAGE_VERSION
+ls -la
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t rewt .
+docker images
+docker tag rewt $DOCKER_USERNAME/rewt:$PACKAGE_VERSION
+docker push $DOCKER_USERNAME/rewt:$PACKAGE_VERSION
