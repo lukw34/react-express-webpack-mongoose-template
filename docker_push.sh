c25881a6ba52ca772af55870f77170c0f5faa797
lukw3 <lukw34@gmail.com>
2018-09-03 22:15:55 +0200

    remove space bug

--- docker_push.sh
+++ docker_push.sh
@@ -2,13 +2,12 @@ PACKAGE_VERSION=$(cat package.json \
   | grep version \
   | head -1 \
   | awk -F: '{ print $2 }' \
-| sed 's/[",]//g')
+| sed 's/[",[:space:]]//g')
 TAGGED_IMAGE=$DOCKER_USERNAME/rewt:$PACKAGE_VERSION
-echo $PACKAGE_VERSION
+echo $PACKAGE_VERSION  
 ls -la
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t rewt .
 docker images
-echo $TAGGED_IMAGE
 docker tag rewt $TAGGED_IMAGE
 docker push $TAGGED_IMAGE
