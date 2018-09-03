4f5d3c943c3e3ee4b0bd515254f07eb6ed389a52
lukw3 <lukw34@gmail.com>
2018-09-03 20:49:27 +0200

    Tag script update

--- docker_push.sh
+++ docker_push.sh
@@ -3,10 +3,12 @@ PACKAGE_VERSION=$(cat package.json \
   | head -1 \
   | awk -F: '{ print $2 }' \
 | sed 's/[",]//g')
+TAGGED_IMAGE=$DOCKER_USERNAME/rewt:$PACKAGE_VERSION
 echo $PACKAGE_VERSION
 ls -la
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t rewt .
 docker images
-docker tag rewt $DOCKER_USERNAME/rewt:$PACKAGE_VERSION
-docker push $DOCKER_USERNAME/rewt:$PACKAGE_VERSION
+echo $TAGGED_IMAGE
+docker tag rewt $TAGGED_IMAGE
+docker push $TAGGED_IMAGE
