6ba01a753166bf0f27ae368181b21747efbb3022
lukw3 <lukw34@gmail.com>
2018-09-04 19:37:58 +0200

    Travis conf refactor

--- docker_push.sh
+++ docker_push.sh
@@ -4,8 +4,6 @@ PACKAGE_VERSION=$(cat package.json \
   | awk -F: '{ print $2 }' \
 | sed 's/[",[:space:]]//g')
 TAGGED_IMAGE=$DOCKER_USERNAME/rewt:$PACKAGE_VERSION
-echo $PACKAGE_VERSION  
-ls -la
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t rewt .
 docker images
