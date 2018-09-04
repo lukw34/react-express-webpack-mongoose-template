3ebf4daae5bd5a8f756f11f28b8efc3b37d59829
Łukasz Walewski <lukw34@gmail.com>
2018-09-04 21:56:46 +0200

    CI/CD (#9)
    Travis configuration for tags deployment

---
+++ docker_push.sh
@@ -0,0 +1,11 @@
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+| sed 's/[",[:space:]]//g')
+TAGGED_IMAGE=$DOCKER_USERNAME/rewt:$PACKAGE_VERSION
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t rewt .
+docker images
+docker tag rewt $TAGGED_IMAGE
+docker push $TAGGED_IMAGE
